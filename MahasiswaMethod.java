9ea50991a22f6f4c140c408df31c54c590219288
Evimeliyani <evimeliyani016@gmail.com>
2025-09-29 21:33:47 +0700

    Add files via upload

---
+++ MahasiswaMethod.java
@@ -0,0 +1,18 @@
+package MethodSetterDanGetter;
+
+public class MahasiswaMethod {
+
+    private String nama;
+
+    // Setter
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    // Getter
+    public String getNama() {
+        return nama;
+    }
+}
+
+
