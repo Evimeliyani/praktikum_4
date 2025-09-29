39896eff8faf92dc9c26c7b0dfb6353a785cbb46
Evimeliyani <evimeliyani016@gmail.com>
2025-09-29 21:34:18 +0700

    Add files via upload

---
+++ MahasiswaMethodTest.java
@@ -0,0 +1,9 @@
+package MethodSetterDanGetter;
+
+public class MahasiswaMethodTest {
+    public static void main(String[] args) {
+        MahasiswaMethod mhs = new MahasiswaMethod();
+        mhs.setNama("Evi Meliyani");
+        System.out.println("Nama: " + mhs.getNama());
+    }
+}
